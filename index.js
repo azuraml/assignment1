8dd6e47322b17ba45c7faa417211316425210fea
Azura ML <lasai.azura.maya@gmail.com>
2023-04-30 23:19:22 -0700

    created accurate authenticated and not main page, loggedin page with username in session, and main page with members page linked and log out

--- index.js
+++ index.js
@@ -7,14 +7,9 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const bcrypt = require('bcrypt');
 const saltRounds = 12;
-
 const port = process.env.PORT || 3000;
-
 const app = express();
-
 const Joi = require("joi");
-
-
 const expireTime = 24 * 60 * 60 * 1000; //expires after 1 day  (hours * minutes * seconds * millis)
 
 /* secret information section */
@@ -23,7 +18,6 @@ const mongodb_user = process.env.MONGODB_USER;
 const mongodb_password = process.env.MONGODB_PASSWORD;
 const mongodb_database = process.env.MONGODB_DATABASE;
 const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
-
 const node_session_secret = process.env.NODE_SESSION_SECRET;
 /* END secret section */
 
@@ -42,18 +36,14 @@ var mongoStore = MongoStore.create({
 
 app.use(session({ 
     secret: node_session_secret,
-	store: mongoStore, //default is memory store 
+	store: mongoStore,  
 	saveUninitialized: false, 
-	resave: true
+	resave: false
 }
 ));
 
 app.get('/', (req,res) => {
-//     res.send("<h1>Hello World!</h1>");
-// });    //do not need this as home page 
-
-// app.get('/', (req,res) => {
-//     //    if (!req.send.session.authenticated) {
+    if (!req.session.authenticated) {
         var html = `
         Welcome
         <div><a href="/signup">Sign Up</a></div>
@@ -61,16 +51,19 @@ app.get('/', (req,res) => {
     `;
     res.send(html);
     return;
-    // }
-// else {
-//     var html =`
-//      Hello, ${req.session.name} !
-//      <div><a href="/members">Members</a></div>
-//      <div><a href="/logout">Log Out</a></div>
-//     `;
-//     res.send(html);
-//     return;
-// }   
+    }
+else {
+     req.session.isAuth = true;
+    console.log(req.session);
+    const username = req.session.username;
+    var html =`
+     Hello, ${username} !
+     <div><a href="/members">Members</a></div>
+     <div><a href="/logout">Log Out</a></div>
+    `;
+    res.send(html);
+    return;
+}   
 });
 
 app.get('/nosql-injection', async (req,res) => {
@@ -85,11 +78,6 @@ app.get('/nosql-injection', async (req,res) => {
 	const schema = Joi.string().max(20).required();
 	const validationResult = schema.validate(username);
 
-	//If we didn't use Joi to validate and check for a valid URL parameter below
-	// we could run our userCollection.find and it would be possible to attack.
-	// A URL parameter of user[$ne]=name would get executed as a MongoDB command
-	// and may result in revealing information about all users or a successful
-	// login without knowing the correct password.
 	if (validationResult.error != null) {  
 	   console.log(validationResult.error);
 	   res.send("<h1 style='color:darkred;'>A NoSQL injection attack was detected!!</h1>");
@@ -103,41 +91,41 @@ app.get('/nosql-injection', async (req,res) => {
     res.send(`<h1>Hello ${username}</h1>`);
 });
 
-app.get('/about', (req,res) => {
-    var color = req.query.color;
+// app.get('/about', (req,res) => {
+//     var color = req.query.color;
 
-    res.send("<h1 style='color:"+color+";'>Patrick Guichon</h1>");
-});
+//     res.send("<h1 style='color:"+color+";'>Patrick Guichon</h1>");
+// });
 
-app.get('/contact', (req,res) => {
-    var missingEmail = req.query.missing;
+// app.get('/contact', (req,res) => {
+//     var missingEmail = req.query.missing;
+//     var html = `
+//         email address:
+//         <form action='/submitEmail' method='post'>
+//             <input name='email' type='text' placeholder='email'>
+//             <button>Submit</button>
+//         </form>
+//     `;
+//     if (missingEmail) {
+//         html += "<br> email is required";
+//     }
+//     res.send(html);
+// });
+
+// app.post('/submitEmail', (req,res) => {
+//     var email = req.body.email;
+//     if (!email) {
+//         res.redirect('/contact?missing=1');
+//     }
+//     else {
+//         res.send("Thanks for subscribing with your email: "+email);
+//     }
+// });
+
+
+app.get('/signup', (req,res) => {
     var html = `
-        email address:
-        <form action='/submitEmail' method='post'>
-            <input name='email' type='text' placeholder='email'>
-            <button>Submit</button>
-        </form>
-    `;
-    if (missingEmail) {
-        html += "<br> email is required";
-    }
-    res.send(html);
-});
-
-app.post('/submitEmail', (req,res) => {
-    var email = req.body.email;
-    if (!email) {
-        res.redirect('/contact?missing=1');
-    }
-    else {
-        res.send("Thanks for subscribing with your email: "+email);
-    }
-});
-
-
-app.get('/createUser', (req,res) => {
-    var html = `
-    create user
+    Sign Up
     <form action='/submitUser' method='post'>
     <input name='username' type='text' placeholder='username'>
     <input name='password' type='password' placeholder='password'>
@@ -222,27 +210,33 @@ app.post('/loggingin', async (req,res) => {
 	}
 });
 
-app.get('/loggedin', (req,res) => {
+app.get('/loggedin', async (req,res) => {
     if (!req.session.authenticated) {
         res.redirect('/login');
     }
-    var html = `
-    You are logged in!
-    `;
-    res.send(html);
+   
+    console.log(req.session);
+    const username = req.session.username;
+
+    res.send(`<h1>Hello ${username}</h1>`);
 });
+
 
 
 
 
 app.get('/logout', (req,res) => {
-	req.session.destroy();
+
+	req.session.destroy((err) => {
+    if (err) {
+        console.log(err);
+    }
     var html = `
-    You are logged out.
+    <p> You are logged out! <p>
     `;
     res.send(html);
 });
-
+});
 
 app.get('/cat/:id', (req,res) => {
 
